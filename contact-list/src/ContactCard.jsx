2880585b1ae0ad996b80d373aa233c1bd6468fab
Geberpraus Torres <kibernum@MacBook-Pro-de-Geberpraus.local>
2025-07-21 13:56:51 -0400

    base

---
+++ contact-list/src/ContactCard.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+
+function Card(props) {
+  return (
+    <div>
+      <div className="card mb-3" style={{ width: "75rem" }}>
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img
+              //src="https://images.pexels.com/photos/32935737/pexels-photo-32935737.jpeg"
+              src={props.image}
+              classNameName="card-img-left rounded-circle"
+              style={{ width: "10rem", height: "10rem" }}
+            />
+          </div>
+          <div className="col-md-4">
+            <div className="card-body">
+              <h5 className="card-title">{props.Name}</h5>
+              <p className="card-text">{props.addres}</p>
+              <p className="card-text">{props.telefono}</p>
+              <p className="card-text">{props.mail}</p>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <button className="btn btn-primary">edit</button>
+            <button className="btn btn-danger">delete</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Card;
--- contact-list/src/views/Home.jsx
+++ contact-list/src/views/Home.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import Card from "../ContactCard";
 
 function Home() {
   return (
@@ -8,29 +9,12 @@ function Home() {
           Add new contact
         </Link>
       </div>
-      <div className="card mb-3" style={{ width: "75rem" }}>
-        <div className="row g-0">
-          <div className="col-md-4">
-            <img
-              src="https://images.pexels.com/photos/32935737/pexels-photo-32935737.jpeg"
-              classNameName="card-img-left rounded-circle"
-              style={{ width: "10rem", height: "10rem" }}
-            />
-          </div>
-          <div className="col-md-4">
-            <div className="card-body">
-              <h5 className="card-title">NOMBRE Contacto</h5>
-              <p className="card-text">Direccion</p>
-              <p className="card-text">telefono</p>
-              <p className="card-text">e-mail</p>
-            </div>
-          </div>
-          <div className="col-md-4">
-            <button className="btn btn-primary">edit</button>
-            <button className="btn btn-danger">delete</button>
-          </div>
-        </div>
-      </div>
+      <Card
+        Name="Contacto 1"
+        addres="direccion 1"
+        teledono="+569 4495 4280"
+        mail="correo@corre.cl"
+      />
     </div>
   );
 }
